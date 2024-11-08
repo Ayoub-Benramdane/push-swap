1a962d1691713d2fa31f9457a6cead22ffdcd575
abenramd <ayoubbenramdane05@gmail.com>
2024-11-08 20:04:26 +0100

    new

--- Push-swap/Functions/push_swap.go
+++ Push-swap/Functions/push_swap.go
@@ -6,36 +6,46 @@ import (
 
 func PushSwap(stackA, stackB []int, instructions []string) ([]string, []int, []int) {
 	if len(stackA) > 3 {
-		min := stackA[0]
-		count := 0
-		for i := 1; i < len(stackA); i++ {
-			if stackA[i] < min {
-				count = i
-				min = stackA[i]
+		if sort.IntsAreSorted(stackA[1:]) {
+			if stackA[0] > stackA[len(stackA)-1] {
+				instructions = append(instructions, "ra")
+				stackA = append(stackA[1:len(stackA)-1], stackA[0], stackA[len(stackA)-1])
+			} else {
+				instructions = append(instructions, "rra", "pb", "ra", "pa", "ra")
+				stackA = append(stackA[1:], stackA[:1]...)
 			}
-		}
-		pos := count
-		if count == 1 && stackA[0] < stackA[2] && stackA[0] > stackA[len(stackA)-1] {
-			instructions = append(instructions, "sa")
-			stackA[0], stackA[1] = stackA[1], stackA[0]
-		} else {
-			for count > 0 && count < len(stackA) {
-				if pos > len(stackA)/2 {
-					instructions = append(instructions, "rra")
-					stackA = append(stackA[len(stackA)-1:], stackA[:len(stackA)-1]...)
-					count++
-				} else {
-					instructions = append(instructions, "ra")
-					stackA = append(stackA[1:], stackA[:1]...)
-					count--
+		} else if !sort.IntsAreSorted(stackA) {
+			min := stackA[0]
+			count := 0
+			for i := 1; i < len(stackA); i++ {
+				if stackA[i] < min {
+					count = i
+					min = stackA[i]
 				}
 			}
-		}
-		stackB = append([]int{stackA[0]}, stackB...)
-		stackA = append([]int{}, stackA[1:]...)
-		instructions = append(instructions, "pb")
-		if !sort.IntsAreSorted(stackA) && len(stackA) > 3 {
+			pos := count
+			if count == 1 && stackA[0] < stackA[2] && stackA[0] > stackA[len(stackA)-1] {
+				instructions = append(instructions, "sa")
+				stackA[0], stackA[1] = stackA[1], stackA[0]
+			} else {
+				for count > 0 && count < len(stackA) {
+					if pos > len(stackA)/2 {
+						instructions = append(instructions, "rra")
+						stackA = append(stackA[len(stackA)-1:], stackA[:len(stackA)-1]...)
+						count++
+					} else {
+						instructions = append(instructions, "ra")
+						stackA = append(stackA[1:], stackA[:1]...)
+						count--
+					}
+				}
+			}
+			stackB = append([]int{stackA[0]}, stackB...)
+			stackA = append([]int{}, stackA[1:]...)
+			instructions = append(instructions, "pb")
+			if !sort.IntsAreSorted(stackA) && len(stackA) > 3 {
 				instructions, stackA, stackB = PushSwap(stackA, stackB, instructions)
+			}
 		}
 	}
 	if len(stackA) < 3 && stackA[1] < stackA[0] {
@@ -59,10 +69,10 @@ func PushSwap(stackA, stackB []int, instructions []string) ([]string, []int, []i
 			stackA[0], stackA[2] = stackA[2], stackA[0]
 			instructions = append(instructions, "sa", "rra")
 		}
-		for i := 0; i < len(stackB); i++ {
-			stackA = append([]int{stackB[i]}, stackA...)
-			instructions = append(instructions, "pa")
-		}
+	}
+	for i := 0; i < len(stackB); i++ {
+		stackA = append([]int{stackB[i]}, stackA...)
+		instructions = append(instructions, "pa")
 	}
 	return instructions, stackA, stackB
 }
